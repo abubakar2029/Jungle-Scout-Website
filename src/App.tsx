0bd5a158ff58158fdf5b5ae48bb551f944854442
Abu Bakar <abubakarzafar2029@gmail.com>
2023-11-14 20:36:42 +0500

    Added margin to div below header

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import Testimonials from "./Components/Testimonials/Testimonials";
 
 function App() {
   return (
-    <div>
+    // <div>
       <BrowserRouter>
         <Header />
         <Routes>
@@ -19,7 +19,7 @@ function App() {
         </Routes>
         <Footer />
       </BrowserRouter>
-    </div>
+    // </div>
   );
 }
 
